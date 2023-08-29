2bde8224b1299c0fc175ce076c5ef5f4ebaf85bb
Dominic Gerhauser <dominicgerhauser1@gmail.com>
2023-08-29 14:26:25 +0200

    -

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 require('dotenv').config();
 const express = require('express');
+const bodyParser = require("body-parser")
 const cors = require('cors');
 const app = express();
+app.use(bodyParser.urlencoded({extended: false}))
 
+let id = 0
+const urls =  new Map()
+urls.set(++id, new URL ("https://www.freecodecamp.org"))
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
@@ -13,12 +18,33 @@ app.use('/public', express.static(`${process.cwd()}/public`));
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
-
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
-});
-
+app.post("/api/shorturl", (req, res) => {
+  const addRes = addUrl(req.body.url)
+  if(addRes === undefined) {
+    return res.json({"error": "invalid url"}).status(400)
+  }
+  const { originalUrl, shortUrl } = addRes
+  res.json({original_url: originalUrl, short_url: shortUrl}).status(200)
+})
+app.get("/api/shorturl/:id", (req, res) =>{
+  res.redirect(urls.get(Number(req.params.id)).toString())
+} )
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
+
+function addUrl(u) {
+  let url
+  try {
+   url = new URL(u) 
+  } catch {
+    return 
+  }
+
+  urls.set(++id, url)
+  console.log(urls)
+  if(!(url.protocol==="http:"||url.protocol==="https:")){
+    return 
+  }
+  return {shortUrl: id, originalUrl: url}
+}
