e96c69d1dd9484ce48c03ec9e8386800e6e5198b
Dominic Gerhauser <dominicgerhauser1@gmail.com>
2023-09-05 02:28:34 +0200

    -

---
+++ frontend/src/components/UrlSubmit.vue
@@ -0,0 +1,17 @@
+<script setup>
+    import { ref } from "vue";
+    import axios from "axios"
+    const url = ref("")
+    async function postUrl() {
+        let res = await axios.get("api/shorturl/8")
+        url.value = ""
+        console.log(res)
+    }
+</script>
+
+<template>
+    <form @submit.prevent>
+    <input v-model="url">
+    <button @click="postUrl">Übermitteln</button>
+    </form>
+</template>
